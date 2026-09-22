004304c6d50294825256493fdcd8acbe243b4e69
salim <salim.belkacemm@protonmail.com>
2026-09-22 19:27:10 +0100

    colapsed lualine confing into one file

--- plugin/appearance.lua
+++ plugin/appearance.lua
@@ -7,7 +7,26 @@ vim.pack.add({
   "https://github.com/nvim-lualine/lualine.nvim",
   "https://github.com/nvim-tree/nvim-web-devicons",
 })
-require("lualine").setup(require("config.lualine"))
+require("lualine").setup({
+  options = {
+    icons_enabled = true,
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = {},
+    lualine_b = {'branch', 'diff', 'diagnostics'},
+    lualine_c = {'filename'},
+    lualine_x = {'filetype'},
+    lualine_y = { 'progress', 'location'},
+    lualine_z = { 'mode'}
+  },
+  inactive_sections = {
+    lualine_b = {'filename'}, lualine_c = {'filetype'},
+    lualine_a = {}, lualine_x = {}, lualine_y = {}, lualine_z = {}
+  },
+})
 
 vim.pack.add({ "https://github.com/luukvbaal/statuscol.nvim" })
 require("statuscol").setup({
